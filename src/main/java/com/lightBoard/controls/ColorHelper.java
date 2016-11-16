30dd37fa9d0423508c19d27d73cc6668b788d3af
Mohammad Al-Hajj <Mohammad_al-Hajj@hotmail.com>
2016-11-16 11:24:28 +0200

    refactored and added comments to ColorHelper class

--- src/main/java/com/lightBoard/controls/ColorHelper.java
+++ src/main/java/com/lightBoard/controls/ColorHelper.java
@@ -3,27 +3,44 @@
  */
 package com.lightBoard.controls;
 
-import java.awt.Color;
+
+import javafx.scene.paint.Color;
 
 /**
  * @author Moham
- *
+ * Library:
+ * Holds Utility functions related to Color
  */
 public class ColorHelper
 {
-	public static final Color SOFT_WHITE = new Color(204,204,204);
-	public static final Color SOFT_BLACK = new Color(51,51,51);
+	public static final Color SOFT_WHITE = new Color(0.9,0.9,0.9, 1);
+	public static final Color SOFT_BLACK = new Color(0.1,0.1,0.1, 1);
 
-	public static Color getForgroundColor(Color backgroundColor)
+	/**
+	 * // FIXME: 11/16/2016 test this method
+	 * returns an appropriate forground depending on the luminosities of the current forground
+	 * and background
+	 * @param backgroundColor
+	 * @return
+	 */
+	public static Color getForgroundColor(Color backgroundColor, Color forgroundColor)
 	{
-		double luminocity = Math.sqrt( 
-				0.299*Math.pow(backgroundColor.getRed(), 2) + 
-				0.587*Math.pow(backgroundColor.getGreen(), 2) + 
-				0.114*Math.pow(backgroundColor.getBlue(), 2));
-		
-		System.out.println(luminocity);
-		Color forground = luminocity > 128 ? ColorHelper.SOFT_BLACK : ColorHelper.SOFT_WHITE;
-		System.out.println(forground);
-		return forground;
+		// get luminosities while taking human eye color weights into consideration
+		double backLuminosity = Math.sqrt(
+			0.299*Math.pow(backgroundColor.getRed(), 2) +
+			0.587*Math.pow(backgroundColor.getGreen(), 2) +
+			0.114*Math.pow(backgroundColor.getBlue(), 2));
+
+		double frontLuminosity = Math.sqrt(
+			0.299*Math.pow(forgroundColor.getRed(), 2) +
+			0.587*Math.pow(forgroundColor.getGreen(), 2) +
+			0.114*Math.pow(forgroundColor.getBlue(), 2));
+
+		// if luminosity difference is more then half the color spectrum, then return the same
+		// forground color
+		if (Math.abs(frontLuminosity - backLuminosity) >= 0.5)
+			return forgroundColor;
+		// else return a default color with luminosity difference of half the color spectrum
+		else return backLuminosity > 0.5 ? ColorHelper.SOFT_BLACK : ColorHelper.SOFT_WHITE;
 	}
 }
